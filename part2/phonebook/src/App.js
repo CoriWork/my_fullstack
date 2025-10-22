40440a81704c6606ee3faafc54eba6a377840adc
CoriWork <492453319@qq.com>
2025-10-22 16:11:28 +0800

    practice 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Filter = ({filterName, handleFilterNameChange}) => {
   return (
@@ -37,15 +38,20 @@ const Persons = ({filteredPersons}) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newPhoneNumber, setNewPhoneNumber] = useState('')
   const [filterName, setFilterName] = useState('')
+
+  useEffect(() => {
+    console.log('useEffect :>> ');
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('response :>> ', response);
+        setPersons(response.data)
+      })
+  }, [])
 
   const filteredPersons = persons.filter(
     person => person.name.toLowerCase().includes(filterName.toLowerCase()))
