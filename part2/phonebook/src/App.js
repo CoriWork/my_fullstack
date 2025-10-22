819727f208b43c45a8c70f4cfd0c80312b557f9a
CoriWork <492453319@qq.com>
2025-10-22 11:37:00 +0800

    practice 2.8

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,25 +2,33 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name : 'Arto Hellas' }
+    { name : 'Arto Hellas' , phone : '040-1234567'}
   ])
   const [newName, setNewName] = useState('')
+  const [newPhoneNumber, setNewPhoneNumber] = useState('')
 
   const handleSubmit = (event) => {
     event.preventDefault()
     if(persons.some(person => person.name === newName)){
       alert(`${newName} is already added to phonebook`)
       setNewName('')
+      setNewPhoneNumber('')
       return
     }
-    const newPersons = persons.concat({name : newName})
+    const newPersons = persons.concat({name : newName, phone : newPhoneNumber})
     setPersons(newPersons)
     setNewName('')
+    setNewPhoneNumber('')
   }
 
   const handleNameChange = (event) => {
     console.log(event.target.value)
     setNewName(event.target.value)
+  }
+
+  const handlePhoneNumberChange = (event) => {
+    console.log(event.target.value);
+    setNewPhoneNumber(event.target.value)
   }
 
   return (
@@ -31,12 +39,15 @@ const App = () => {
           name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
+          number: <input value={newPhoneNumber} onChange={handlePhoneNumberChange} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <div>
-        {persons.map(person => <p key={person.name}>{person.name}</p>)}
+        {persons.map(person => <p key={person.name}>{person.name} : {person.phone}</p>)}
       </div>
     </div>
   )
