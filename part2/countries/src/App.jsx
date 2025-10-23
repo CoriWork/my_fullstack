58c40d4b4c9a7d1c88c5eee77f96ab70d96aa37e
CoriWork <492453319@qq.com>
2025-10-23 23:45:56 +0800

    practice 2.20

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import countryServices from './services/countryServices'
+import axios from "axios";
 
 const Filter = ({filterValue, value, handleFilterValueChange}) => {
   return (
@@ -36,6 +37,44 @@ const CountryCard = ({country}) => {
   )
 }
 
+const WeatherCard = ({city, latlng}) => {
+  const [weather, setWeather] = useState(null)
+  const [isCityAvailable, setIsCityAvailable] = useState(true)
+  const api_key = import.meta.env.VITE_WEATHER_KEY
+  const baseUrl = 'https://api.openweathermap.org/data/2.5/weather?'
+
+  useEffect(() => {
+    if (!city && latlng.length !== 2){
+      setIsCityAvailable(false)
+      return
+    }
+
+    axios.get(`${baseUrl}q=${city}&appid=${api_key}&units=metric`)
+    .then(response => {
+      setWeather(response.data)
+    })
+  }, [city, latlng])
+
+  if (!city && latlng.length !== 2) setIsCityAvailable(false)
+  console.log('1')
+  if (!isCityAvailable) return null
+  console.log('2')
+  if (!weather) return null
+  console.log('3')
+  const temperature = weather.main.temp
+  const iconCode = weather.weather[0].icon
+  const weatherIconUrl = iconCode ? `https://openweathermap.org/img/wn/${iconCode}@2x.png` : null
+  const windSpeed = weather.wind.speed
+  return (
+    <div>
+      <h2>{`Weather in ${city}`}</h2>
+      <p>{`Temperature ${temperature} Celsius`}</p>
+      <img src={weatherIconUrl} alt={`weather icon`}/>
+      <p>{`Wind ${windSpeed} m/s`}</p>
+    </div>
+  )
+}
+
 const CountryDisplay = ({countryList, handleShowChange}) => {
   if(countryList.length > 10){
     return (
@@ -47,7 +86,10 @@ const CountryDisplay = ({countryList, handleShowChange}) => {
   else if(countryList.length === 1){
     const country = countryList[0];
     return (
-      <CountryCard country={country} />
+      <div>
+        <CountryCard country={country} />
+        <WeatherCard city={country.capital[0]} latlng={country.capitalInfo.latlng} />
+      </div>
     )
   }
   else {
